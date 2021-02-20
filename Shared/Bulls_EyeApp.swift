487456d77239d82551381667684fe4eefa043232
muhammadhurali <muhammad.hur@salsoft.net>
2021-02-20 13:24:51 +0500

    Initial Commit

---
+++ Shared/Bulls_EyeApp.swift
@@ -0,0 +1,17 @@
+//
+//  Bulls_EyeApp.swift
+//  Shared
+//
+//  Created by Hur Ali on 20/02/2021.
+//
+
+import SwiftUI
+
+@main
+struct Bulls_EyeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
