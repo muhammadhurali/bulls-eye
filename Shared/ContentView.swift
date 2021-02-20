ebf2273ad3d856864ed35340867a280ec7cacdc0
muhammadhurali <muhammad.hur@salsoft.net>
2021-02-20 16:54:04 +0500

    comitting project to git

--- Shared/ContentView.swift
+++ Shared/ContentView.swift
@@ -8,14 +8,50 @@
 import SwiftUI
 
 struct ContentView: View {
+    @State private var alertIsVisible: Bool = false
+    @State private var sliderValue: Double = 50.0
+    
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+        VStack {
+            Text("🎯🎯🎯\nPUT THE BULLSEYE AS CLOSE AS YOU CAN TO")
+                .bold()
+                .kerning(2.0)
+                .multilineTextAlignment(.center)
+                .lineSpacing(4)
+                .font(.footnote)
+            
+            Text("89")
+                .kerning(-1.0)
+                .font(.largeTitle)
+                .fontWeight(.black)
+               
+            
+            HStack {
+                Text("1")
+                    .bold()
+                    
+                Slider(value: self.$sliderValue, in: 1.0...100.0 )
+                
+                Text("100")
+                    .bold()
+            }
+            Button(action: {
+                self.alertIsVisible = true
+            }) {
+                Text("Hit Me")
+            }
+            .alert(isPresented: self.$alertIsVisible, content: {
+                return Alert(title: Text("Hello"), message: Text("Slider's value is \(self.sliderValue)"), dismissButton: .default(Text("OK")))
+            })
+        }
+
     }
 }
 
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
         ContentView()
+            
+        ContentView().previewLayout(.fixed(width: 568, height: 320))
     }
 }
